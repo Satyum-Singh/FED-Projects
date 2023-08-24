2022c548ed97b66025a5893f29073a0a01207929
Satyum <satyumspathania@gmail.com>
2023-08-24 14:07:20 +0530

    how to ask ques?

--- Javascript Projects/Chat/script.js
+++ Javascript Projects/Chat/script.js
@@ -1,1 +1,35 @@
-const sendChatBtn = document.getElementById(".chat-input span");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage;
+
+const handleChat = () => {
+  userMessage = chatInput.value.trim();
+  console.log(userMessage);
+
+  if (userMessage !== "") {
+    // create a div element
+    const newChat = document.createElement("div");
+    newChat.classList.add("chat-bubble");
+    newChat.classList.add("user");
+
+    // create a p element
+    const newChatText = document.createElement("p");
+    newChatText.classList.add("chat-message");
+    newChatText.innerText = userMessage;
+
+    // append p element to div element
+    newChat.appendChild(newChatText);
+
+    // append div element to chat window
+    const chatWindow = document.querySelector(".chat-window");
+    chatWindow.appendChild(newChat);
+
+    // clear chat input
+    chatInput.value = "";
+  } else {
+    return;
+  }
+};
+
+sendChatBtn.addEventListener("click", handleChat);
